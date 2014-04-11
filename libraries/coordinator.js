cdf6af84022fe19a33da2fc6810d4b3fdc878077
Alexander Savelyev <savelyev@psu.edu>
2014-04-11 19:46:54 -0400

    Now using URIs to distinguish between components
    Replaced callbackNo with a more generic URI property.

--- libraries/coordinator.js
+++ libraries/coordinator.js
@@ -1,34 +1,24 @@
 "use strict";
 
-//
-// TODO:
-// 
-// 2. Build a function that would add .coordinationMetadata to components automatically.
-// 
-// 3. Make components use .eventTypes
-//
-
 var coordinator = {
-
+    
     // List of components to link
-    componentList: [],
-
-    // Callback counter
-    callbackNo: 0
+    componentList: []
 };
 
 coordinator.eventTypes = {
     
-    // Dictionary of event types
-    select:     "select",
-    highlight:  "highlight",
-    filter:     "filter"
+    // Dictionary of event types (for convenience only)
+    select:                 "select",
+    highlight:              "highlight",
+    filter:                 "filter",
+    userProfileLocation:    "userProfileLocation"
 };
 
 coordinator.signUp = function (component) {
 
-    // Add component to the "to do" list
-    if (coordinator.componentList.indexOf(component) < 0){
+    // Add component to the "to link" list
+    if (coordinator.componentList.indexOf(component) < 0) {
         coordinator.componentList.push(component);
     }
 };
@@ -56,7 +46,7 @@ coordinator.linkComponents = function () {
 
                 // If so, register them as subscribers with the current component.
                 if (callbackFunction) {
-                    coordinator.registerEvent(currentComponent, eventType, callbackFunction);
+                    coordinator.registerEvent(currentComponent, eventType, callbackFunction, nextComponent.coordinationMetadata.URI);
                 }
             }
 
@@ -67,7 +57,7 @@ coordinator.linkComponents = function () {
 
                 // If so, register the current component as a subscriber with all of them.
                 if (callbackFunction) {
-                    coordinator.registerEvent(nextComponent, eventType, callbackFunction);
+                    coordinator.registerEvent(nextComponent, eventType, callbackFunction, currentComponent.coordinationMetadata.URI);
                 }
             }
 
@@ -77,52 +67,54 @@ coordinator.linkComponents = function () {
     // end of .linkComponents()
 };
 
-coordinator.registerEvent = function (component, type, callbackFunction) {
-    
-    // This method registers callback functions for events broadcast by 
-    // the specified component.
+coordinator.registerEvent = function (broadcastingComponent, eventType, callbackFunction, callbackComponentURI) {
 
-    var registeredCallbacks = component.coordinationMetadata.triggers[type];
+    // Copy callback functions into the "triggers" property of the broadcastingComponent.
+
+    var registeredCallbacks = broadcastingComponent.coordinationMetadata.triggers[eventType];
 
     if (registeredCallbacks) {
-
-        // registeredCallbacks[callbackFunction] = callbackFunction;
-
-        registeredCallbacks["callback" + this.callbackNo] = callbackFunction;
-        this.callbackNo++;
+        registeredCallbacks[callbackComponentURI] = callbackFunction;
     }
-    
+
 };
 
+
+//
+// Misc
+//
+
 coordinator.triggerEvent = function (type, tweetIds, flag) {
-    
-    // This is a generic method that specific triggers within components 
-    // should use to fire events. To keep UI responsive, this method 
-    // sets a delay before the events are fired.
-    
+
+    // This is a generic method that can be re-used by specific components 
+    // in order to trigger events. This method is provided for convenience only
+    // and is not a part of the coordination framework.
+
     // 'this' must be bound to the calling component before use.
-    
+
+
     //////////////////// Naive (immediate) coordination ////////////////////////
-    
-   var registeredCallbacks = this.coordinationMetadata.triggers[type];
 
-   if (registeredCallbacks) {
+//    var registeredCallbacks = this.coordinationMetadata.triggers[type];
+//
+//    if (registeredCallbacks) {
+//
+//        var event = {
+//            type:       type,       // event type, e.g. XXX, select, highlight, filter, etc.
+//            tweetIds:   tweetIds,   // IDs of the tweets affected by this event
+//            flag:       flag        // direction of the action, e.g. select / deselect
+//        };
+//
+//        var callbackFunction;
+//
+//        for (callbackFunction in registeredCallbacks) {
+//            registeredCallbacks[callbackFunction](event);
+//        }
+//    }
 
-       var event = {
-           "type":         type,       // event type, e.g. XXX, select, highlight, filter, etc.
-           "tweetIds":     tweetIds,   // IDs of the tweets affected by this event
-           "flag":         flag        // direction of the action, e.g. select / deselect
-       };
 
-       var callbackFunction;
-
-       for (callbackFunction in registeredCallbacks) {
-           registeredCallbacks[callbackFunction](event);
-       }
-   }
-    
     //////////////////// Coordination with a fixed delay ///////////////////////
-    
+
 //    var timeoutDuration = 1;
 //    
 //    if (type === coordinator.eventTypes.select) {
@@ -135,60 +127,63 @@ coordinator.triggerEvent = function (type, tweetIds, flag) {
 //    window.setTimeout(function () {
 //        timeoutTrigger(type, tweetIds, flag);
 //    }, timeoutDuration);
-    
+
+
     /////////////////////////// Timed coordination /////////////////////////////
-    
-    // var registeredCallbacks = this.coordinationMetadata.triggers[type];
 
-    // if (registeredCallbacks) {
+    var registeredCallbacks = this.coordinationMetadata.triggers[type];
 
-    //     var event = {
-    //         "type":         type,       // event type, e.g. XXX, select, highlight, filter, etc.
-    //         "tweetIds":     tweetIds,   // IDs of the tweets affected by this event
-    //         "flag":         flag        // direction of the action, e.g. select / deselect
-    //     };
+    if (registeredCallbacks) {
 
-    //     var timedTrigger = coordinator.timedTriggerEvent.bind(this);            // We're "double-binding" - both triggerEvent and triggerEventNow 
-    //                                                                             // are now bound to the calling component.
-        
-    //     timedTrigger(registeredCallbacks, event);
-    // }
+        var event = {
+            type:       type,       // event type, e.g. XXX, select, highlight, filter, etc.
+            tweetIds:   tweetIds,   // IDs of the tweets affected by this event
+            flag:       flag        // direction of the action, e.g. select / deselect
+        };
+
+        var timedTrigger = coordinator.timedTriggerEvent.bind(this);            // We're "double-binding" - both triggerEvent and triggerEventNow 
+                                                                                // are now bound to the calling component.
+
+        timedTrigger(registeredCallbacks, event);
+    }
 
 };
 
 coordinator.timedTriggerEvent = function (registeredCallbacks, eventData) {
-    
-    var keys    = Object.keys(registeredCallbacks);
-    var length  = keys.length;
-    var idx     = 0;
-    var delay   = 25;
-    
+
+    var keys = Object.keys(registeredCallbacks),
+        length = keys.length,
+        idx = 0,
+        delay = 25;
+
     var timedRunFunction = function () {
-        
+
         // This function should get all of its data-related variables through 
         // closure, otherwise the inner setTimeout() call won't work.
-        
+
         var start = Date.now();
-        
+
         do {
             registeredCallbacks[keys[idx]](eventData);
             idx++;
         } while (idx < length && (Date.now() - start < 50));
 
-        if (idx < length){
+        if (idx < length) {
             setTimeout(timedRunFunction, delay);
         }
     }
-
-    setTimeout(timedRunFunction, delay);    
     
+    if (length > 0) {
+        setTimeout(timedRunFunction, delay);
+    }
+
 };
 
-coordinator.triggerEventNow = function (type, tweetIds, flag) {
-    
+coordinator.triggerEventNow = function(type, tweetIds, flag) {
+
     // This is a generic method that specific triggers within components 
     // should use to fire events.
-    
+
     // 'this' must be bound to the calling component before use.
 
     var registeredCallbacks = this.coordinationMetadata.triggers[type];
@@ -196,9 +191,9 @@ coordinator.triggerEventNow = function (type, tweetIds, flag) {
     if (registeredCallbacks) {
 
         var event = {
-            "type":         type,       // event type, e.g. XXX, select, highlight, filter, etc.
-            "tweetIds":     tweetIds,   // IDs of the tweets affected by this event
-            "flag":         flag        // direction of the action, e.g. select / deselect
+            type:       type,       // event type, e.g. XXX, select, highlight, filter, etc.
+            tweetIds:   tweetIds,   // IDs of the tweets affected by this event
+            flag:       flag        // direction of the action, e.g. select / deselect
         };
 
         var callbackFunction;
